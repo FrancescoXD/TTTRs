5f63a0a00d8c7f159bfafe483717ef42fe914c98
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2021-06-30 23:10:27 +0200

    first release

---
+++ src/main.rs
@@ -0,0 +1,99 @@
+use std::{io::stdin};
+
+fn main() {
+    println!("Tic Tac Toe Game");
+    let player1 = ask_player_char();
+    let player2 = ask_player_char();
+
+    let mut turn= 0;
+
+    let mut table= [' '; 9];
+    show_board(&table);
+
+    let mut empty_slot: u8 = 9;
+    
+    loop {
+        show_board(&table[..]);
+        let symbol = select_player(&mut turn, player1, player2);
+        insert_symbol(&mut empty_slot, symbol, &mut table[..], &mut turn);
+        if check_win(symbol, empty_slot, &mut table[..]) {
+            break println!("{} won!", symbol);
+        }
+    }
+}
+
+fn insert_symbol(empty_slot: &mut u8, symbol: char, table: &mut [char], turn: &mut i32) {
+    let mut pos = String::new();
+
+    println!("Insert the position:");
+    stdin().read_line(&mut pos).expect("Error while getting input.");
+    let mut pos: usize = pos.trim().parse().expect("Error while parsing the number.");
+
+    if pos != 0 {
+        pos -= 1;
+    }
+
+    if is_valid_position(pos, &table[..]) {
+        table[pos] = symbol;
+        *turn += 1;
+        *empty_slot -= 1;
+    }
+}
+
+fn ask_player_char() -> char {
+    println!("Insert player symbol: ");
+
+    let mut c = String::new();
+    stdin().read_line(&mut c).expect("Error while getting input.");
+
+    c.chars().next().unwrap()
+}
+
+fn select_player(turn: &mut i32, player1: char, player2: char) ->  char {
+    if *turn % 2 == 0 {
+        return player1;
+    }
+
+    player2
+}
+
+fn show_board(table: &[char]) {
+    for (i, &item) in table.iter().enumerate() {
+        print!("{} ", item);
+        if i == 2 || i == 5 || i == 8 {
+            println!();
+        }
+    }
+}
+
+fn is_valid_position(pos: usize, table: &[char]) -> bool {
+    if table[pos] == ' ' {
+        return true;
+    }
+
+    false
+}
+
+fn check_win(symbol: char, empty_slot: u8, table: &mut [char]) -> bool {
+    if empty_slot == 0 {
+        return false;
+    } else if table[0] == symbol && table[1] == symbol && table[2] == symbol {
+        return true;
+    } else if table[3] == symbol && table[4] == symbol && table[5] == symbol {
+        return true;
+    } else if table[6] == symbol && table[7] == symbol && table[8] == symbol {
+        return true;
+    } else if table[0] == symbol && table[3] == symbol && table[6] == symbol {
+        return true;
+    } else if table[1] == symbol && table[4] == symbol && table[7] == symbol {
+        return true;
+    } else if table[2] == symbol && table[5] == symbol && table[8] == symbol {
+        return true;
+    } else if table[0] == symbol && table[4] == symbol && table[8] == symbol {
+        return true;
+    } else if table[2] == symbol && table[4] == symbol && table[6] == symbol {
+        return true;
+    }
+
+    false
+}
